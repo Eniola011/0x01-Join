c14bae10ee898740901acd54bcb389bcef71eb7b
Eniola011 <yettyagbalu@gmail.com>
2024-06-15 05:07:04 +0100

    still developing...

--- users/views.py
+++ users/views.py
@@ -9,3 +9,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+def index(request):
+    """ HomePage """
+    return render(request, 'home.html')
