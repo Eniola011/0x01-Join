079e4c73df6abcbc22f962e18061d7b0fcae3a82
Eniola011 <yettyagbalu@gmail.com>
2024-06-15 04:47:51 +0100

    still developing...

--- join/urls.py
+++ join/urls.py
@@ -24,3 +24,10 @@ urlpatterns = [
     path('', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
